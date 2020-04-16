bc88c6b22210f0cc69015b3314055be772183827
Cameron <cst1493@gmail.com>
2020-04-16 00:43:12 -0500

    Completed locally.  Might change ghetto front end if I have time.

---
+++ CST/PHP&MySQL/books.php
@@ -0,0 +1,38 @@
+<?php
+    include_once 'includes/dbh.php'; //filepath.
+    include 'includes/shortcuts.php';
+    //http://localhost/CMPS_294_WebDev/CST/PHP&MySQL/books.php connect locally. TODO remove
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> books.php </title>
+    </head>
+    <body>
+        <h3> <b>Data View.  Exit to return back to the menu.</b> </h3>
+        <?php
+            $userInputArr = array($_POST['title'], $_POST['author'],
+            $_POST['ISBN'], $_POST['publisher'], $_POST['year']);
+            
+            //find the correct operation.
+            if( strcmp($_POST['dbCommand'], 'add') == 0 )
+            {
+                addToDB($connection, $userInputArr);
+            }
+            if( strcmp($_POST['dbCommand'], 'search_all') == 0 )
+            {
+                printResult($connection, "SELECT * FROM books;");
+            }
+            if( strcmp($_POST['dbCommand'], 'delete') == 0 )
+            {
+                $deleteTitle = $userInputArr[0];
+                deleteFromDB($connection, $deleteTitle);
+            }
+            if( strcmp($_POST['dbCommand'], 'edit') == 0 )
+            {
+                editBookByTitle($connection, $userInputArr);
+            }
+        ?>
+    </body>
+</html>
---
+++ CST/PHP&MySQL/includes/shortcuts.php
@@ -0,0 +1,80 @@
+<?php
+    
+    function printResult($connection, $sqlCommand) //or printResult???
+    {
+        $result = mysqli_query($connection, $sqlCommand); //$conn comes from dbh.php, the database handler.
+        
+        $resultCheck = mysqli_num_rows($result); //if data exists, expect greater than 0.
+        if ($resultCheck > 0) //if any entries exist, print them out.
+        {
+            while ($row = mysqli_fetch_assoc($result)) //while the database has more entries...
+            {
+                echo $row['title'] . ", " . $row['author'] . ", " . $row['isbn'] . ", " .
+                $row['publisher'] . ", " . $row['year'] . "; " . "<br>";
+            }
+        }
+        else echo("No entries found.");
+    }
+
+    function addToDB($connection, $userInputArr)
+    {
+        $foundDuplicate = false;
+        $result = mysqli_query($connection, "SELECT title FROM books;");
+        $titleDuplicateCheck = mysqli_num_rows($result);
+        if ($titleDuplicateCheck > 0) //if any entries exist, check for duplicates
+        {
+            while ($row = mysqli_fetch_assoc($result)) //while the database has more entries...
+            {
+                if( strcmp($userInputArr[0], $row['title']) == 0 ){
+                    $foundDuplicate = true;
+                }
+            }
+        }
+        if ($foundDuplicate)
+        {
+            echo "The title, \"$userInputArr[0],\" already exists.  Try another title or check if that book is already in the database.";
+        }
+        else //finally add to database.
+        {
+            //SQL syntax: insert into books (title, author, isbn, publisher, year) VALUES ('The Book', 'Will Smith', 1234, 'The Publishers',  1999);
+            $sqlCommand = "insert into books (title, author, isbn, publisher, year) VALUES 
+            ('$userInputArr[0]', '$userInputArr[1]', '$userInputArr[2]', '$userInputArr[3]',  '$userInputArr[4]');";
+            
+            $result = mysqli_query($connection, $sqlCommand);
+            echo "Successfully added new entry: <br>$userInputArr[0], $userInputArr[1], $userInputArr[2], $userInputArr[3], $userInputArr[4]; ";
+        }
+    }
+
+    function deleteFromDB($connection, $deleteTitle)
+    {
+        $sqlCommand = "SELECT * FROM books WHERE title = '$deleteTitle';"; //check if any match exists.
+        $result = mysqli_query($connection, $sqlCommand);
+        
+        $deleteTitleCheck = mysqli_num_rows($result);
+        if ($deleteTitleCheck > 0)
+        {
+            $sqlCommand = "DELETE FROM books WHERE books.title = '$deleteTitle';"; //delete entry
+            $result = mysqli_query($connection, $sqlCommand);
+            echo "sucessfully deleted $deleteTitle from the database.";
+        }
+        else echo("No entries found.");
+    }
+
+    function editBookByTitle($connection, $userInputArr)
+    {   //title[0], author[1], isbn[2], publisher[3], year[4]
+        $editTitle = $userInputArr[0];
+        $sqlCommand = "SELECT * FROM books WHERE title = '$editTitle';"; //check if title exists.
+        $result = mysqli_query($connection, $sqlCommand);
+        
+        $editTitleCheck = mysqli_num_rows($result);
+        if ($editTitleCheck > 0){
+            $sqlCommand = "UPDATE books SET author = '$userInputArr[1]', isbn = '$userInputArr[2]',
+            publisher = '$userInputArr[3]', year = '$userInputArr[4]' WHERE title = '$editTitle';";
+            $result = mysqli_query($connection, $sqlCommand); //update
+
+            echo "sucessfully changed \"$editTitle\".";
+        }
+        else echo("No entries found.");
+    }
+
+    
